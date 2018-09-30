a1a0613b1cd784601a075acb193b5783422fdc3f
Bas van den Boom <basvandenboom3@gmail.com>
2018-09-30 17:56:14 +0200

    Code om te kijken naar de intensiteit van één bepaalde frequentie uitgezet tegen de tijd

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from scipy.io import wavfile
 import numpy as np
 
 
-#Fourier Transform
-sample_freq, signal = wavfile.read('Audio Samples/audiocheck.net_400Hz_-5dBFS_1500Hz_-17dBFS_1s.wav')
+# Fourier Transform
+sample_freq, signal = wavfile.read('Audio Samples/01_This_Is_War_(Main_Theme).wav')
+signal = signal.T[0]
 intensity, freqs, time, im = plt.specgram(signal, NFFT=1024, Fs=sample_freq, noverlap=64)
 
-#Getting right values for one time interval
+# Getting right values for one time interval
+freq_index = 13
+
 presence = []
 print intensity.size
-for row in intensity:
-    presence.append(10* np.log10(row[:1])) #Apply logarithmic scale, because dB
-
-#Plot graph with intensity-frequency
-plt.plot(freqs, presence)
-plt.axis([0, 2000, 0, 100]) #Only to 2000 Hz, because test signal only has 400Hz and 1500Hz frequency
+row = intensity[freq_index]
+for timefreq in row:
+    presence.append(10 * np.log10(timefreq))  # Apply logarithmic scale, because dB
+# Plot graph with intensity-frequency
+plt.clf()
+plt.plot(time, presence)
+plt.title("Intensiteit bij tijd voor " + str(freqs[freq_index]) + "Hz")
+plt.axis([0, 170, 0, 100])
 plt.show()
