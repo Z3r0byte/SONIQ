b2350f9d65b3f11d18cdf1ddde35bf0d8c24fa5b
Bas van den Boom <basvandenboom3@gmail.com>
2018-09-30 17:13:15 +0200

    Code voor plotten fourier transformatie waarden op een bepaald tijdsinterval bij de bijbehorende frequentie

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-import time
+import numpy as np
 
-sample_freq, signal = wavfile.read('Audio Samples/01_This_Is_War_(Main_Theme)-short.wav')
 
-NFFT = 512
-fig, (ax2) = plt.subplots(1)
-Pxx, freqs, bins, im = ax2.specgram(signal, NFFT=NFFT, Fs=sample_freq, noverlap=256)
+#Fourier Transform
+sample_freq, signal = wavfile.read('Audio Samples/audiocheck.net_400Hz_-5dBFS_1500Hz_-17dBFS_1s.wav')
+intensity, freqs, time, im = plt.specgram(signal, NFFT=1024, Fs=sample_freq, noverlap=64)
+
+#Getting right values for one time interval
+presence = []
+print intensity.size
+for row in intensity:
+    presence.append(10* np.log10(row[:1])) #Apply logarithmic scale, because dB
+
+#Plot graph with intensity-frequency
+plt.plot(freqs, presence)
+plt.axis([0, 2000, 0, 100]) #Only to 2000 Hz, because test signal only has 400Hz and 1500Hz frequency
 plt.show()
-
-
-fig, (ax2) = plt.subplots(1)
-sample_freq, signal = wavfile.read('Audio Samples/01_This_Is_War_(Main_Theme)-record.wav')
-Pxx, freqs, bins, im = ax2.specgram(signal, NFFT=NFFT, Fs=sample_freq, noverlap=256)
-plt.show()
-
