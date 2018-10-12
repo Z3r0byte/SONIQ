5fed2142af244a0b547aad060306f6c5b4d76abf
Bas van den Boom <basvandenboom3@gmail.com>
2018-10-12 19:28:56 +0200

    Parameter aangemaakt voor minimale amplitude

--- fingerprinting/peaks.py
+++ fingerprinting/peaks.py
@@ -1,11 +1,11 @@
-def find_peaks(spectrum, xwindow, ywindow):
+def find_peaks(spectrum, xwindow, ywindow, min_amp=10):
     """
 
     :param spectrum: the 2-D spectrum array
     :param xwindow: search-window in time in which to search for higher values
     :param ywindow: search-window in frequency in which to search for higher values
     :return:
-    :rtype: 2-D array of peaks in spectrum
+    :rtype: 2-D array(freq, time) of peaks in spectrum
     """
     peaks = []
     freq_length = spectrum[0].size
@@ -13,7 +13,7 @@ def find_peaks(spectrum, xwindow, ywindow):
     for spectrum_row in range(0, spectrum_length):  # select row
         freq_array = spectrum[spectrum_row]
         for freq_array_index in range(0, freq_length):  # select point in row to test if peak
-            if freq_array[freq_array_index] < 0.1:
+            if freq_array[freq_array_index] < min_amp:
                 continue
             for spectrum_row2 in range(-ywindow, ywindow + 1):  # select test-row to check for higher values
                 index_sum = spectrum_row + spectrum_row2
