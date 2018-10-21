55dc2ec614b0c7f851ddede0a69d5fb47b0d122d
Bas van den Boom <basvandenboom3@gmail.com>
2018-10-21 13:57:15 +0200

    Ondersteuning voor CLI argumenten

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+from __future__ import print_function
 from config import AUDIO_DIR
 import files.filehandler as files
 import tests.test as tests
+import sys
+import getopt
 
-# files.fingerprint_all(AUDIO_DIR)
 
-tests.run_tests("Audio Samples/Opnames")
+def help():
+    print("SONIQ - ruisbestendige audio herkenning")
+    print("Gebruik: main.py [optie]")
+    print("    -t test_map              Draai tests met fragmenten in de opgegeven map")
+    print("    -f                       Indexeer alle .wav bestanden in AUDIO_DIR uit config.py")
+    print("    -s                       Server modus")
+    exit()
+
+
+if not len(sys.argv[1:]):
+    help()
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "t:fs")
+except getopt.GetoptError as error:
+    print(str(error))
+    help()
+
+for opt, arg in opts:
+    if opt == "-t":
+        tests.run_tests(arg)
+    elif opt == "-f":
+        if len(arg) >= 1:
+            AUDIO_DIR = arg
+        files.fingerprint_all(AUDIO_DIR)
+    elif opt == "-s":
+        print("WIP")
+    else:
+        help()
