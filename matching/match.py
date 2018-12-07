376ceacb3b056753fe6bd6e5a826d6b8de036773
Bas van den Boom <basvandenboom3@gmail.com>
2018-12-07 20:16:45 +0100

    Comments toegevoegd match functie

--- matching/match.py
+++ matching/match.py
@@ -9,38 +9,53 @@ import collections
 
 
 def match(signal, cursor=None):
+    """
+    Matcht het gegeven signaal aan een signaal in de database
+    :param signal: het signaal om te matchen
+    :param cursor: de database verbinding
+    :return: [succes (boolean)], [confidences //van alle gevonden liedjes// (array(int))], [confidence //van de match// (int)], [resultaat_zin (string)], [titel (string)], [artiest (string)]
+    """
     start_time = t.time()
-    intensity, freqs, time = fourier.apply_fourier(signal, NFFT_WINDOW, SAMPLE_FREQ, N_OVERLAP)
-    peaks_array = peaks.find_peaks(intensity, PEAK_TIME_WINDOW, PEAK_FREQ_WINDOW)
-    hashes = fingerprint.fingerprint(peaks_array, FINGERPRINT_TIME_WINDOW)
+    intensity, freqs, time = fourier.apply_fourier(signal, NFFT_WINDOW, SAMPLE_FREQ,
+                                                   N_OVERLAP)  # Fourier-transformatie toepassen
+    peaks_array = peaks.find_peaks(intensity, PEAK_TIME_WINDOW, PEAK_FREQ_WINDOW)  # Pieken zoeken
+    hashes = fingerprint.fingerprint(peaks_array, FINGERPRINT_TIME_WINDOW)  # Vingerafdrukken maken van pieken
 
     if len(hashes) == 0:
-        return False, [[0, 0]], 0, "Unable to generate hashes. Not enough peaks? No match.", (t.time() - start_time)
+        return False, [[0, 0]], 0, "Unable to generate hashes. Not enough peaks? No match.", (
+                    t.time() - start_time), "Unknown", "Unknown"
 
     fingerprint_data = []
     fingerprint_dictionary = {}
+    # Opslaan tijd van elke vingerafdruk in een dictionary variabele
     for hash in hashes:
         fingerprint_data.append(hash[0])
         fingerprint_dictionary[hash[0]] = hash[1]
+    # De 10 liedjes met de meeste overeenkomstige vingerafdrukken uit de database halen gesorteerd van meeste naar minste overeenkomsten
     if cursor is not None:
         fingerprint_match_count = dbhelper.get_songs_with_fingerprints(fingerprint_data, cursor)
     else:
         fingerprint_match_count = dbhelper.get_songs_with_fingerprints(fingerprint_data)
 
     if len(fingerprint_match_count) == 0:
-        return False, [[0, 0]], 0, "Did not find any similarities in database. No match.", (t.time() - start_time)
+        return False, [[0, 0]], 0, "Did not find any similarities in database. No match.", (
+                    t.time() - start_time), "Unknown", "Unknown"
 
     confidences = []
+    # Voor elk van de tien liedjes uit de database de confidence waarde berekenen
     for match in fingerprint_match_count:
+        # Alle tijdstippen per vingerafdruk voor een liedje ophalen
         if cursor is not None:
             offsets = dbhelper.get_times_for_fingerprints_of_song(fingerprint_data, match[0], cursor)
         else:
             offsets = dbhelper.get_times_for_fingerprints_of_song(fingerprint_data, match[0])
 
         differences = []
+        # Tijdsverschil berekenen tussen tijdstip in database en tijdstip in het te matchen signaal
         for offset in offsets:
             differences.append(round(fingerprint_dictionary[str(offset[1]).lower()] - offset[2], -1))
 
+        # Confidence waarde berekenen gebaseerd op het aantal keer dat een specifiek tijdsverschil voorkomt
         diff_freqs = collections.Counter(differences)
         total = 0
         for diff_freq in diff_freqs.values():
@@ -50,10 +65,12 @@ def match(signal, cursor=None):
                 total += pow(diff_freq, 2)
         confidence = [match[0], float(total) / len(offsets)]
         confidences.append(confidence)
+
         if confidence[1] >= MIN_CONFIDENCE_FOR_STOP:
             break
     confidences.sort(key=lambda x: x[1])
     confidences.reverse()
+    # Informatie over het liedje ophalen met de grootste confidence waarde
     if cursor is not None:
         matched_song = dbhelper.get_song_by_id(confidences[0][0], cursor)[0]
     else:
