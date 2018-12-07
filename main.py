80e5bc7da1c47220142e43428b45138c9c6434b0
Bas van den Boom <basvandenboom3@gmail.com>
2018-12-07 20:37:56 +0100

    Comments toegevoegd main

--- main.py
+++ main.py
@@ -15,11 +15,9 @@ def help():
     print("SONIQ - Ruisbestendige audio-herkenning")
     print("Gebruik: main.py [optie]")
     print("    -t test_map              Draai tests met fragmenten in de opgegeven map")
-    print(
-        "    -n test_map              Draai tests met automatisch gegenereerde (optie -g) bestanden in de opgegeven map")
+    print("    -n test_map              Draai tests met automatisch gegenereerde (optie -g) bestanden in de opgegeven map")
     print("    -f                       Indexeer alle .wav bestanden in AUDIO_DIR uit config.py")
-    print(
-        "    -g                       Genereer n testbestanden uit AUDIO_DIR met ruis NOISE_SOURCE_FILE (SNR = -6, -3, 0, 3, 6 db)")
+    print("    -g                       Genereer n testbestanden uit AUDIO_DIR met ruis NOISE_SOURCE_FILE (SNR = -6, -3, 0, 3, 6 db)")
     print("    -s                       Server modus")
     exit()
 
@@ -33,6 +31,8 @@ except getopt.GetoptError as error:
     print(str(error))
     help()
 
+
+# De opgegeven argumenten verwerken
 for opt, arg in opts:
     if opt == "-t":
         tests.run_tests(arg)
