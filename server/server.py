6da549a2a305c90127be52fc64c1b856d0694035
Bas van den Boom <basvandenboom3@gmail.com>
2018-12-07 20:21:19 +0100

    Comments toegevoegd server

--- server/server.py
+++ server/server.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from config import SERVER_HOST, SERVER_PORT, SAMPLE_FREQ, HOST, DATABASE, USER, PASSWORD
 from flask import Flask, jsonify, request
 from flaskext.mysql import MySQL
@@ -9,6 +10,7 @@ import numpy as np
 
 app = Flask(__name__)
 
+# Voorbreiden database verbinding
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = USER
 app.config['MYSQL_DATABASE_PASSWORD'] = PASSWORD
@@ -19,6 +21,10 @@ mysql.init_app(app)
 
 @app.route("/search/new", methods=["GET"])
 def generate_search_id():
+    """
+    Nieuwe zoekopdracht aanmaken en id terugsturen
+    :return:
+    """
     files.cleanup()
     search_id = str(uuid.uuid4())
     files.create_search_file(search_id)
@@ -27,6 +33,11 @@ def generate_search_id():
 
 @app.route("/songs/<song_id>", methods=["GET"])
 def get_songs(song_id):
+    """
+    Titel en artiest geven van een liedje
+    :param song_id: het id van het liedje, stuurt de informatie terug van alle liedjes indien dit "all" is
+    :return:
+    """
     if song_id == "all":
         songs = dbhelper.get_all_songs()
     else:
@@ -39,8 +50,13 @@ def get_songs(song_id):
 
 @app.route("/search/data", methods=["POST"])
 def process_data():
+    """
+    Verwerkt geüploade data
+    :return:
+    """
     search_id = request.headers.get("SEARCH_ID")
     print "Processing request " + search_id + "..."
+    # Controleren search_id
     if search_id is None:
         return "{ \"error\" : \"Geen zoek-id opgegeven\"}", 403
     try:
@@ -50,14 +66,16 @@ def process_data():
     if not files.search_file_exists(search_id):
         return "{ \"error\" : \"Zoekopdracht verlopen of nooit aangevraagd\"}", 403
 
+    # Geüploade data toevoegen aan al ontvangen data
     file_data = files.read_save_file(search_id)
-    if file_data.size > (SAMPLE_FREQ * 11):
+    if file_data.size > (SAMPLE_FREQ * 10):
         return "{ \"error\" : \"Zoekopdracht te groot\"}", 403
     post_data = request.get_json(force=True)
     audio_array = np.array(post_data['data'], dtype=np.int16)
     total_array = np.append(file_data, audio_array)
     files.save_search_file(search_id, total_array)
 
+    # Totaal ontvangen opname matchen en antwoord terug sturen
     success, confidences, song_id, result, time, title, artist = match.match(total_array, mysql.get_db().cursor())
     response = {"success": success, "confidence": confidences[0][1], "song_id": song_id, "result": result,
                 "time": time, "song": {"title": title, "artist": artist}}
@@ -66,6 +84,11 @@ def process_data():
 
 @app.errorhandler(505)
 def internal_server_error(e):
+    """
+    Handelt eventuele fouten netjes af met een 500 error code
+    :param e:
+    :return:
+    """
     return "{\"error\": \"Er is een fout opgetreden tijdens het verwerken van je verzoek\"}", 500
 
 
